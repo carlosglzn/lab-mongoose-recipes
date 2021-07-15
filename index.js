768ffec8d47bec159dd4ffcb98f79aa0103f6339
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-07-15 17:19:29 -0500

    Lab done

--- index.js
+++ index.js
@@ -20,8 +20,58 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    /*
+    Recipe.create(recipeExample, (error, newRecipe) => {
+      if (error) {
+        console.log('An error happened:', error);
+        return;
+      }
+      console.log('The recipe is saved and its value is: ', newRecipe);
+    }) */
+
+    Recipe.insertMany(data, (error, newRecipeArr) => {
+      if (error) {
+        console.log('An error happened: ', error);
+        return;
+      }
+      newRecipeArr.forEach(element => {
+        console.log(element.title);
+      })
+    })
+
+    Recipe.findOneAndUpdate( {title: "Rigatoni alla Genovese"}, {duration: 100})
+      .then(() => console.log("Rigatoni duration updated"))
+
+
+    Recipe.deleteOne({ title: "Carrot Cake" })
+      .then(() => console.log("Carrot Cake recipe deleted"))
+      .then(() => mongoose.disconnect())
+
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+
+  const recipeExample = {
+    "title": "Orange and Milk-Braised Pork Carnitas",
+    "level": "UltraPro Chef",
+    "ingredients": [
+      "3 1/2 pounds boneless pork shoulder, cut into large pieces",
+      "1 tablespoon freshly ground black pepper",
+      "1 tablespoon kosher salt, or more to taste",
+      "2 tablespoons vegetable oil",
+      "2 bay leaves",
+      "2 teaspoons ground cumin",
+      "1 teaspoon dried oregano",
+      "1/4 teaspoon cayenne pepper",
+      "1 orange, juiced and zested"
+    ],
+    "cuisine": "American",
+    "dishType": "main_course",
+    "image": "https://images.media-allrecipes.com/userphotos/720x405/2280918.jpg",
+    "duration": 160,
+    "creator": "Chef John"
+  }
